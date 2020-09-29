d16860a7583f6d9252f4cd643b0775c07acbb62b
Christof Wollenhaupt <christof.wollenhaupt@foxpert.com>
2020-09-29 18:41:44 +0200

    Code migrated to new project

---
+++ fpDotNet/DotNetContainer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Microsoft.Win32;
+using Westwind.WebConnection;
+
+namespace fpDotNet
+{
+    [ClassInterface(ClassInterfaceType.None)]
+    [ComVisible(true)]
+    [Guid("AD735B97-BA14-49A3-B326-34D8F1058B73")]
+    public partial class DotNetContainer : UserControl, IDotNetContainer
+    {
+        public DotNetContainer()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// This container hosts any WinForm control.
+        /// </summary>
+        /// <param name="control">Reference to the WinForm control</param>
+        public void SetControl(Object control)
+        {
+            var uiControl = (Control) control;
+            Controls.Clear();
+            Controls.Add(uiControl);
+            uiControl.Dock = DockStyle.Fill;
+            uiControl.Visible = true;        
+        }
+
+        /// <summary>
+        /// Returns a reference to an wwDotNetBridge instance. This instance is used
+        /// instead of one create in ClrHost.dll, because COM InterOp runs in its own
+        /// AppDomain in VFP. We make sure we create all controls in the AppDomain
+        /// that contains the WinForm container.
+        /// </summary>
+        /// <returns>Store the wwDotNetBridge instance in the oDotNetBridge property</returns>
+        public Object CreateBridge()
+        {
+            var bridge = new wwDotNetBridge();
+            return bridge;
+        }
+
+        /// <summary>
+        /// VFP won't add a COM object to a form unless it has a Control key. 
+        /// </summary>
+        /// <param name="keyName"></param>
+        [ComRegisterFunction]
+        public static void RegisterClass(string keyName)
+        {
+            using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(keyName.Replace(@"HKEY_CLASSES_ROOT\", ""), true))
+                key?.CreateSubKey("Control")?.Close();
+        }
+
+        [ComUnregisterFunction]
+        public static void UnregisterClass(string keyName)
+        {
+            using (RegistryKey key = Registry.ClassesRoot.OpenSubKey(keyName.Replace(@"HKEY_CLASSES_ROOT\", ""), true))
+                key?.DeleteSubKey("Control", false);
+        }
+    }
+}
---
+++ fpDotNet/IDotNetContainer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace fpDotNet
+{
+    /// <summary>
+    /// interface to host a single .NET control
+    /// </summary>
+    ///<remarks>
+    /// This is the interface that want to make visible to Visual FoxPro
+    /// applications. COM requires an extra interface definition for every type
+    /// that is used in a COM interface definition. VFP can be a bit
+    /// picky about what types it works with.
+    ///
+    /// Therefore we define everything as type Object here. This helps Visual FoxPro
+    /// to deal with the interface and minimizes the amount of class libraries we
+    /// have to generate. VFP doesn't care, because it's not strongly typed anyway.
+    ///
+    /// The actual objects used will all be .NET objects that are packaged in
+    /// a COM Callable Wrapper.
+    /// </remarks>
+    [ComVisible(true)]
+    public interface IDotNetContainer
+    {
+        void SetControl(Object control);
+        Object CreateBridge();
+    }
+}                                                       
